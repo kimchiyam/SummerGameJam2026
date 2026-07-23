7aeb0e8618c699be8b85c3b6275b03a17687ba68
seriously5252-design <seriously5252@gmail.com>
2026-07-23 13:57:54 +0900

    정리좀

--- Assets/AJH/01.Scripts/Upgrate.cs
+++ Assets/AJH/01.Scripts/Upgrate.cs
@@ -1,33 +1,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum StatType
+[CreateAssetMenu(fileName = "Upgrate", menuName = "Scriptable Objects/Upgrate")]
+public class Upgrate : ScriptableObject
+{
+
+}
+
+public enum State
 {
     AttackDamage,
-    MoveSpeed,
-    Defense,
-    ProductionSpeed,
-    PickupRange,
-    PickupChance
+    Speed,
+    Defance,
+    Intelligence,
+
+    QicklyEarn,
+    PickUp
 }
 
 [System.Serializable]
 public class StatModifier
 {
-    public StatType statType;
+    public State statType;
     public float valuePerLevel;
 }
 
-[CreateAssetMenu(fileName = "State", menuName = "SO/State")]
+[CreateAssetMenu(fileName = "UpGrade", menuName = "SO/UpgradeSO")]
 public class UpgradeSO : ScriptableObject
 {
     [Header("기본 정보")]
-    public string _upgradeName;
-    public Sprite _icon;
+    public string upgradeName;
+    public Sprite icon;
     [TextArea(2, 4)] public string description;
 
     [Header("레벨")]
-    [Min(1)] public int _maxLevel = 5;
+    [Min(1)] public int maxLevel = 5;
 
     [Header("스탯 증가량")]
     [Tooltip("레벨당 증가하는 스탯 목록")]
