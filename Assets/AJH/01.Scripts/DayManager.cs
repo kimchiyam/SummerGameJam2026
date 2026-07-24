11d22de3771bcf7d14f20470d843c19a4ae85259
seriously5252-design <seriously5252@gmail.com>
2026-07-24 14:19:47 +0900

    날짜 끝

--- Assets/AJH/01.Scripts/DayManager.cs
+++ Assets/AJH/01.Scripts/DayManager.cs
@@ -1,61 +1,48 @@
-using System;
 using UnityEngine;
 
 public class DayManager : MonoBehaviour
 {
+    public static DayManager instance { get; private set; }
+
     [SerializeField] Timer timer;
     [SerializeField] DayUI dayUI;
 
     int _currentPlanetDay = 1;
     int _totalDay = 1;
-    [SerializeField]float _currentTime = 360f;
-
-    public event Action OnEndDay;
-    public event Action OnStartDay;
-
-    public static DayManager Instance { get; private set; }
 
     private void Awake()
     {
-        if (Instance != null && Instance != this)
+        if (instance != null && instance != this)
         {
             Destroy(gameObject);
             return;
         }
-        Instance = this;
+
+        instance = this;
         DontDestroyOnLoad(gameObject);
     }
 
     private void Start()
     {
-        Debug.Log("[DayManager] Start - 게임 시작");
         timer.OnTimerEnd += EndDay;
         StartNextDay();
     }
 
+    public void PauseTime() => timer.Pause();
+    public void ResumeTime() => timer.Resume();
+
     private void StartNextDay()
     {
-        Debug.Log($"[DayManager] StartNextDay - 총 {_totalDay}일차 / 행성 내 {_currentPlanetDay}일차");
-        dayUI.Show(_totalDay);
-        timer.StartTimer(360f);
+        dayUI.Show(_totalDay, () => timer.StartTimer(360f));
     }
 
     void EndDay()
     {
-        Debug.Log($"[DayManager] EndDay - {_totalDay}일차 종료");
-
         _totalDay++;
-
         if (_currentPlanetDay >= 2)
-        {
             _currentPlanetDay = 1;
-            Debug.Log("[DayManager] 행성 나갈 시간! 다음 행성 1일차로 리셋");
-        }
         else
-        {
             _currentPlanetDay++;
-            Debug.Log($"[DayManager] 같은 행성 계속 - 행성 내 {_currentPlanetDay}일차로");
-        }
 
         StartNextDay();
     }
--- Assets/AJH/01.Scripts/DayUI.cs
+++ Assets/AJH/01.Scripts/DayUI.cs
@@ -1,3 +1,4 @@
+// DayUI - 애니메이션 끝나면 알려주기만
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
@@ -5,9 +6,9 @@ using UnityEngine;
 public class DayUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _dayUI;
-    [SerializeField] float _hidePosY = 300f;
-    [SerializeField] float _showPosY =0f;
-    
+    [SerializeField] float _hidePosY = 700f;
+    [SerializeField] float _showPosY = 0f;
+
     RectTransform _rect;
 
     private void Awake()
@@ -15,14 +16,15 @@ public class DayUI : MonoBehaviour
         _rect = _dayUI.rectTransform;
     }
 
-    public void Show(int Day)
+    public void Show(int Day, System.Action onComplete = null)
     {
         _dayUI.text = $"{Day}일차";
-        _rect.anchoredPosition = new Vector2(0,_hidePosY);
+        _rect.anchoredPosition = new Vector2(0, _hidePosY);
 
         Sequence seq = DOTween.Sequence();
         seq.Append(_rect.DOAnchorPosY(_showPosY, 0.5f).SetEase(Ease.OutBack));
         seq.AppendInterval(2f);
         seq.Append(_rect.DOAnchorPosY(_hidePosY, 0.5f).SetEase(Ease.InBack));
+        seq.OnComplete(() => onComplete?.Invoke());  // 끝나면 콜백 호출
     }
 }
