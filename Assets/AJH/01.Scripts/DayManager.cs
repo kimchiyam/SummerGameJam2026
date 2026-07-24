a9e47cf51114477e2c20487751fce723e25e1494
seriously5252-design <seriously5252@gmail.com>
2026-07-24 12:17:35 +0900

    시간 완료

--- Assets/AJH/01.Scripts/DayManager.cs
+++ Assets/AJH/01.Scripts/DayManager.cs
@@ -3,30 +3,60 @@ using UnityEngine;
 
 public class DayManager : MonoBehaviour
 {
-    Timer timer;
+    [SerializeField] Timer timer;
+    [SerializeField] DayUI dayUI;
+
     int _currentPlanetDay = 1;
     int _totalDay = 1;
+    [SerializeField]float _currentTime = 360f;
 
     public event Action OnEndDay;
     public event Action OnStartDay;
 
+    public static DayManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
     private void Start()
     {
+        Debug.Log("[DayManager] Start - 게임 시작");
         timer.OnTimerEnd += EndDay;
+        StartNextDay();
+    }
+
+    private void StartNextDay()
+    {
+        Debug.Log($"[DayManager] StartNextDay - 총 {_totalDay}일차 / 행성 내 {_currentPlanetDay}일차");
+        dayUI.Show(_totalDay);
         timer.StartTimer(360f);
     }
 
     void EndDay()
     {
+        Debug.Log($"[DayManager] EndDay - {_totalDay}일차 종료");
+
         _totalDay++;
-        if(_currentPlanetDay >= 2)
+
+        if (_currentPlanetDay >= 2)
         {
             _currentPlanetDay = 1;
+            Debug.Log("[DayManager] 행성 나갈 시간! 다음 행성 1일차로 리셋");
         }
         else
         {
             _currentPlanetDay++;
-            timer.StartTimer(360);
+            Debug.Log($"[DayManager] 같은 행성 계속 - 행성 내 {_currentPlanetDay}일차로");
         }
+
+        StartNextDay();
     }
 }
--- Assets/AJH/01.Scripts/Timer.cs
+++ Assets/AJH/01.Scripts/Timer.cs
@@ -1,11 +1,11 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Timer : MonoBehaviour
 {
-    float _timeLength;
-    float _currentTime;
-
+    [SerializeField]float _timeLength;
+    [SerializeField]float _currentTime;
     bool _isPlaying;
 
     public event Action OnTimerEnd;
@@ -14,25 +14,52 @@ public class Timer : MonoBehaviour
     {
         _isPlaying = true;
         _timeLength = time;
+        _currentTime = 0f;
+        Debug.Log($"[Timer] StartTimer - {time}초 타이머 시작");
     }
 
     public void Pause()
     {
         _isPlaying = false;
+        Debug.Log($"[Timer] Pause - {_currentTime:F2}초에서 정지");
     }
 
     public void Resume()
     {
         _isPlaying = true;
+        Debug.Log($"[Timer] Resume - {_currentTime:F2}초에서 재개");
     }
 
     private void Update()
     {
+        // ===== 테스트용 단축키 =====
+        var kb = Keyboard.current;
+        if (kb != null)
+        {
+            // Space: 일시정지 / 재개
+            if (kb.spaceKey.wasPressedThisFrame)
+            {
+                if (_isPlaying) Pause();
+                else Resume();
+            }
+
+            // E: 즉시 하루 끝내기
+            if (kb.eKey.wasPressedThisFrame)
+            {
+                Debug.Log("[Timer] 테스트: 강제 종료");
+                _currentTime = _timeLength;
+            }
+        }
+
+        // Shift 누르면 10배속
+        float multiplier = (kb != null && kb.leftShiftKey.isPressed) ? 10f : 1f;
+
         if (!_isPlaying) return;
-        _currentTime += Time.deltaTime;
-        if(_currentTime >= _timeLength)
+        _currentTime += Time.deltaTime * multiplier;
+        if (_currentTime >= _timeLength)
         {
             _isPlaying = false;
+            Debug.Log($"[Timer] 타이머 종료! ({_timeLength}초 경과)");
             OnTimerEnd?.Invoke();
         }
     }
