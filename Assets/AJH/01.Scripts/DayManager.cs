80b6ae9e444c5fab0d35a905f48a2f81dbb45c72
seriously5252-design <seriously5252@gmail.com>
2026-07-25 04:59:15 +0900

    FadeInFadeOut

--- Assets/AJH/01.Scripts/DayManager.cs
+++ Assets/AJH/01.Scripts/DayManager.cs
@@ -1,4 +1,5 @@
 using KSM.Scripts.Planet;
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,10 +8,15 @@ public class DayManager : MonoBehaviour
 {
     public static DayManager Instance { get; private set; }
 
+    [Header("여러가지")]
     [SerializeField] Timer timer;
     [SerializeField] DayUI dayUI;
     [SerializeField] MiddleResult middleResultUI;
     [SerializeField] int scenePlent;
+
+    [Header("페이드인")]
+    [SerializeField] CanvasGroup fadeCanvas;   // 필드 추가, 인스펙터에서 연결
+    [SerializeField] float fadeDuration = 0.7f;
 
     int _currentPlanetDay = 1;
     int _totalDay = 13;
@@ -65,21 +71,7 @@ public class DayManager : MonoBehaviour
 
     void ProceedAfterDay()
     {
-        if (_currentPlanetDay >= 2)
-        {
-            _currentPlanetDay = 1;
-            var psm = FindObjectOfType<PlanetSceneManager>();
-            if (psm != null)
-            {
-                Debug.Log("[Day] OnPlanetCleared 호출");
-                psm.OnPlanetCleared();
-            }
-        }
-        else
-        {
-            _currentPlanetDay++;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+        StartCoroutine(ProceedAfterDayRoutine());
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
@@ -98,5 +90,49 @@ public class DayManager : MonoBehaviour
         {
             StartNextDay();
         }
+    }
+
+    IEnumerator ProceedAfterDayRoutine()
+    {
+        bool goingToPlanet = (_currentPlanetDay >= 2);
+
+        if (goingToPlanet)
+            yield return Fade(0f, 1f);   // 행성씬 갈 때만 어두워짐
+
+        if (goingToPlanet)
+        {
+            _currentPlanetDay = 1;
+            var psm = FindObjectOfType<PlanetSceneManager>();
+            if (psm != null) psm.OnPlanetCleared();
+        }
+        else
+        {
+            _currentPlanetDay++;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        yield return null;
+
+        if (goingToPlanet)
+            yield return Fade(1f, 0f);
+    }
+
+    IEnumerator Fade(float from, float to)
+    {
+        if (fadeCanvas == null) yield break;
+
+        fadeCanvas.gameObject.SetActive(true);   // 시작할 때 켜기
+        fadeCanvas.alpha = from;
+
+        float t = 0f;
+        while (t < fadeDuration)
+        {
+            t += Time.unscaledDeltaTime;
+            fadeCanvas.alpha = Mathf.Lerp(from, to, t / fadeDuration);
+            yield return null;
+        }
+        fadeCanvas.alpha = to;
+
+        if (to <= 0f) fadeCanvas.gameObject.SetActive(false);   // 다 밝아지면 끄기
     }
 }
